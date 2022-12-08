95404f953fac6898c3436101a540fe7f37503237
Darwin <darwin@email.su>
2022-12-08 12:07:29 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://nufeen.github.io/ph/sw.js', { scope: 'https://nufeen.github.io/ph/' })})}
