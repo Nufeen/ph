8b9c09a93480ac4a5be2ce64952e8acde353e5fa
Darwin <darwin@MacBook-Pro-Darwin.local>
2024-09-14 23:26:35 +0300

    Add transit retrograde marks

--- src/interface/Zodiac/TransitPlanets/index.tsx
+++ src/interface/Zodiac/TransitPlanets/index.tsx
@@ -3,6 +3,7 @@ import {useContext} from 'react'
 import s from './index.module.css'
 
 import {CelestialContext} from '../../../CelestialContext.js'
+import {SettingContext} from '../../../SettingContext'
 
 const {sin, cos} = Math
 
@@ -22,6 +23,8 @@ const icons = {
 
 export default function TransitPlanets({zero, x0, y0}) {
   const {transitHoroscope} = useContext(CelestialContext)
+
+  const {settings} = useContext(SettingContext)
   let l = 126
 
   function deg(x) {
@@ -31,24 +34,27 @@ export default function TransitPlanets({zero, x0, y0}) {
 
   return (
     <>
-      {transitHoroscope.CelestialBodies.all.map(planet => (
-        <>
-          <circle
-            className={s.transit}
-            cx={x0 + (l - 56) * sin(deg(planet))}
-            cy={y0 + (l - 56) * cos(deg(planet))}
-            r="1"
-          />
-          <text
-            className={s.text}
-            x={x0 + (l + 11) * sin(deg(planet)) - 3}
-            y={y0 + (l + 12) * cos(deg(planet)) + 2}
-            fontWeight={400}
-          >
-            {icons[planet.label]}
-          </text>
-        </>
-      ))}
+      {transitHoroscope.CelestialBodies.all
+        .filter(planet => settings?.objects?.planets[planet?.label])
+        .map(planet => (
+          <>
+            <circle
+              className={s.transit}
+              cx={x0 + (l - 56) * sin(deg(planet))}
+              cy={y0 + (l - 56) * cos(deg(planet))}
+              r="1"
+            />
+            <text
+              className={s.text}
+              x={x0 + (l + 11) * sin(deg(planet)) - 3}
+              y={y0 + (l + 12) * cos(deg(planet)) + 2}
+              fontWeight={400}
+            >
+              {icons[planet.label]}
+              {planet.isRetrograde && <tspan dy="2">R</tspan>}
+            </text>
+          </>
+        ))}
     </>
   )
 }
--- src/interface/Zodiac/index.tsx
+++ src/interface/Zodiac/index.tsx
@@ -21,6 +21,8 @@ import TransitPlanets from './TransitPlanets/index.js'
 
 import Planets from './Planets/index.js'
 
+import TransitAspects from './TransitAspects/index.js'
+
 const {sin, cos, abs} = Math
 
 const l = [...Array(12).keys()]
@@ -253,68 +255,6 @@ function Fictive({calendarDay, zero, x0, y0, lat, lng}) {
           />
         </>
       )}
-    </>
-  )
-}
-
-function TransitAspects({calendarDay, zero, x0, y0}) {
-  const {horoscope, transitHoroscope} = useContext(CelestialContext)
-
-  const {settings} = useContext(SettingContext)
-
-  const threshold = settings.interface.aspectOrb ?? 4
-
-  const A = horoscope.CelestialBodies.all.map(
-    x => x.ChartPosition.Ecliptic.DecimalDegrees
-  )
-  const B = transitHoroscope.CelestialBodies.all.map(
-    x => x.ChartPosition.Ecliptic.DecimalDegrees
-  )
-
-  const AT = []
-
-  A.forEach(a => {
-    B.forEach(b => {
-      const aspect = aspectBetween(a, b)
-      if (aspect) {
-        AT.push(aspect)
-      }
-    })
-  })
-
-  function aspectBetween(a, b) {
-    const d = abs(a - b)
-
-    if (d < 50 || d > 125) return null
-
-    if (d % 30 < threshold) {
-      return {
-        a,
-        b,
-        d: d % 30
-      }
-    }
-
-    return null
-  }
-
-  function deg(x) {
-    return ((x + zero) * 3.14) / 180
-  }
-
-  return (
-    <>
-      {AT.map(({a, b, d}) => (
-        <line
-          key={JSON.stringify([a, b])}
-          x1={x0 + 70 * sin(deg(a))}
-          y1={y0 + 70 * cos(deg(a))}
-          x2={x0 + 70 * sin(deg(b))}
-          y2={y0 + 70 * cos(deg(b))}
-          stroke={abs(a - b) % 45 < 3 ? 'red' : 'deepskyblue'}
-          strokeWidth={d < 1 ? 1 : 0.3}
-        />
-      ))}
     </>
   )
 }
