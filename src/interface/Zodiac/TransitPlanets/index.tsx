4975c8958734366b1d94e9af858d27cf6cd1687e
Darwin <darwin@email.su>
2025-02-09 21:34:33 +0300

    Something alike collision detect for outer circle

--- src/interface/Zodiac/TransitPlanets/index.tsx
+++ src/interface/Zodiac/TransitPlanets/index.tsx
@@ -22,57 +22,72 @@ export default function TransitPlanets({zero, x0, y0}) {
     return ((y + zero) * Math.PI) / 180
   }
 
+  const P = (
+    settings.chartType == 'progressed'
+      ? progressedHoroscope
+      : transitHoroscope
+  ).CelestialBodies.all
+    .filter(planet => settings?.objects?.planets[planet?.label])
+    .map(planet => ({
+      planet,
+      name: planet.label,
+      lng: deg(planet),
+      x: x0 - 4 + (l + 11) * sin(deg(planet)),
+      y: y0 + 3 + (l + 10) * cos(deg(planet))
+    }))
+    .sort((a, b) => b.lng - a.lng)
+
+  // kind of naive collision detection
+  for (let i = 0; i < P.length - 1; i++) {
+    if (
+      Math.abs(P[i + 1].y - P[i].y) < 5 &&
+      P[i + 1].x - P[i].x < 5
+    ) {
+      P[i + 1].x += 5
+    }
+  }
+
   return (
     <>
-      {(settings.chartType == 'progressed'
-        ? progressedHoroscope
-        : transitHoroscope
-      ).CelestialBodies.all
-        .filter(planet => settings?.objects?.planets[planet?.label])
-        .map(planet => (
-          <React.Fragment key={planet?.label}>
-            <circle
-              className={s.transit}
-              cx={x0 + (l - 56) * sin(deg(planet))}
-              cy={y0 + (l - 56) * cos(deg(planet))}
-              r="1"
-            />
+      {P.map(({name, lng, planet, x, y}) => (
+        <React.Fragment key={name}>
+          <circle
+            className={s.transit}
+            cx={x0 + (l - 56) * sin(lng)}
+            cy={y0 + (l - 56) * cos(lng)}
+            r="1"
+          />
 
-            <circle
-              className={s.transit}
-              cx={x0 + (l + 4) * sin(deg(planet))}
-              cy={y0 + (l + 4) * cos(deg(planet))}
-              r="1"
-            />
+          <circle
+            className={s.transit}
+            cx={x0 + (l + 4) * sin(lng)}
+            cy={y0 + (l + 4) * cos(lng)}
+            r="1"
+          />
 
-            <text
-              className={s.text}
-              x={x0 + (l + 11) * sin(deg(planet)) - 3}
-              y={y0 + (l + 12) * cos(deg(planet)) + 2}
-              fontWeight={400}
-            >
-              {icons[planet.label]}
-              {planet.isRetrograde && <tspan dy="2">R</tspan>}
-            </text>
+          <text className={s.text} x={x} y={y} fontWeight={400}>
+            {icons[planet.label]}
+            {planet.isRetrograde && <tspan dy="2">R</tspan>}
+          </text>
 
-            <text
-              className={s.text}
-              x={x0 + (l + 11) * sin(deg(planet)) + 2}
-              y={y0 + (l + 12) * cos(deg(planet)) - 4}
-              fontWeight={400}
-            >
-              {settings.interface.planetAngles && (
-                <tspan dy="0">
-                  {
-                    planet.ChartPosition.Ecliptic.ArcDegreesFormatted30.split(
-                      ' '
-                    )[0]
-                  }
-                </tspan>
-              )}
-            </text>
-          </React.Fragment>
-        ))}
+          <text
+            className={s.text}
+            x={x + 4}
+            y={y - 5}
+            fontWeight={400}
+          >
+            {settings.interface.planetAngles && (
+              <tspan dy="0">
+                {
+                  planet.ChartPosition.Ecliptic.ArcDegreesFormatted30.split(
+                    ' '
+                  )[0]
+                }
+              </tspan>
+            )}
+          </text>
+        </React.Fragment>
+      ))}
     </>
   )
 }
